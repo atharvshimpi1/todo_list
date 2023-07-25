18b6f8e4edc4041093315f516acb8428a89db5a8
Atharv shimpi <139147982+atharvshimpi1@users.noreply.github.com>
2023-07-25 10:41:16 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Todo from './component/todo';
+
+function App() {
+  return (
+   <div>
+    <Todo/>
+   </div>
+  );
+}
+
+export default App;
